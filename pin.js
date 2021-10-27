7b18d661c496f53788419167fcafee8ff527e773
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-10-27 16:38:22 -0700

    Create pin.js

---
+++ pin.js
@@ -0,0 +1,18 @@
+/* Instructions */
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// If the function is passed a valid PIN string, return true, else return false.
+
+function validatePIN (pin) {
+  if ((pin.split('').length === 4) || (pin.split('').length === 6)) {
+    let split = pin.split('')
+    for (i = 0; i < pin.split('').length; i++) {
+      if ((isNaN(split[i])) || (split[i] === ' ') || (split[i] === '\n')) {
+        return false
+      }
+    }
+    return true
+  } else { return false }
+}
+
+validatePIN(`123
+`)
