ed6d01c2d7c44e6179dece31449d3bfe901e6590
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-11-01 18:22:53 -0700

    More stuff

--- parityOutlier.js
+++ parityOutlier.js
@@ -1,4 +1,4 @@
-/*  */
+/* Finished November 1st, 2021 */
     // You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
     // Examples
@@ -14,13 +14,13 @@ function findOutlier(integers) { // Check if there are more odds or evens in the
     });
     console.log(integers, arr);
     for (i = 0; i < arr.length; i++) {
-        if ((i = 0) && (arr[i] != arr[i+1])) {
-            return integers[i];     
-        } else if ((i = arr.length - 1) && (arr[i] != arr[i-1])) {
+        if ((i == 0) && (arr[i] != arr[i+1]) && (arr[i] != arr[i+2])) {
             return integers[i];
-        } else if ((arr[i] != arr[i-1]) && (arr[i] != arr[i+1])) {
+        } else if ((i == arr.length - 1) && (arr[i] != arr[i-1])) {
+            return integers[i];
+        } else if ((i != 0) && (arr[i] != arr[i-1]) && (arr[i] != arr[i+1])) {
             return integers[i];
         }
     }
 }
-findOutlier([161, 3, 1719, 18, 11, 13, -21]);
+findOutlier([160, 5, 1718, 18, 10, 12, -20]);
